ed40e253d554bba0dfe6060e29641e02b8a15a09
swapnilk30 <sk9137587676@gmail.com>
2024-06-28 22:04:09 +0530

    .

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,7 @@ public class ProductController {
 	
 	
 	//products
+	@GetMapping
 	public ResponseEntity<List<Product>> getAllProducts(){
 		return ResponseEntity.ok(productService.getAllProduct());
 	}
